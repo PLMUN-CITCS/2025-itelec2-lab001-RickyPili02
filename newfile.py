1aa5d4f2f9c86866a13d8681b162e93668220a9c
RickyPili02 <>
2025-02-12 19:05:50 +0800

    Last Sync: 2025-02-12 11:05:50 (Mobile)

---
+++ newfile.py
@@ -0,0 +1,8 @@
+name = "Ricky Pili"
+course = "Bachelor Of Science In Information Technology"
+interests = "Programming"
+
+print("Hello world!")
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Interests in programming: {interests}")
