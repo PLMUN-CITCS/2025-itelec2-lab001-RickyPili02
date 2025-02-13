323ab242560f0cc071253e7f2a022302f9407888
RickyPili02 <>
2025-02-13 16:28:22 +0800

    Last Sync: 2025-02-13 08:28:22 (Mobile)

--- Hello.py
+++
@@ -1,8 +0,0 @@
-name = "Ricky Pili"
-course = "Bachelor Of Science In Information Technology"
-interests = "Programming"
-
-print("Hello world!")
-print(f"Name: {name}")
-print(f"Course: {course}")
-print(f"Interests in programming: {interests}")
