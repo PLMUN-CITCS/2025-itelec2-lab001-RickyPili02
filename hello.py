0adecbd19a8fcbb623b485903c718cf3d6af9fd2
RickyPili02 <>
2025-02-13 17:48:41 +0800

    Last Sync: 2025-02-13 09:48:41 (Mobile)

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello world!")  # Print "Hello world!"
+
+# Open and read the intro.txt file
+with open("intro.txt", "r") as file:
+    print(file.read().strip())  # Print the contents of intro.txt)
